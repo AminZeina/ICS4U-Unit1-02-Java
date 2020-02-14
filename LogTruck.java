e21724bdc3a5dcab7fa92502deae9ea6a9c76686
CitrusMelon <amin.zeina@ocsbstudent.ca>
2020-02-14 01:45:36 +0000

    added comments

--- LogTruck.java
+++ LogTruck.java
@@ -14,12 +14,17 @@ public class LogTruck {
   static int numberOfLogs;
   
   public static void main(String[] args) {
+    // Create scanner object
     Scanner userInput = new Scanner(System.in);
+    // Ask user for input
     System.out.println("Are the logs 0.25, 0.5, or 1 metre?");
     System.out.print("Please enter 0.25, 0.5, or 1: ");
+    // read user input
     lengthOfLogs = userInput.nextDouble();
+    // Calculate # of logs 
     weight =  lengthOfLogs * 20;
     numberOfLogs =  1100 / (int) weight;
+    // Tell user how many logs the truck can carry
     System.out.println("The truck can carry " + numberOfLogs + " logs that are " 
         + lengthOfLogs + "m long.");
   }
